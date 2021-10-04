5c5065044fa12ec11f720900811254b80851131d
Chris Johnson <jhnsn.c@gmail.com>
2021-10-03 19:59:22 -0500

    allow invocation with existing element for first parameter

--- test/index.js
+++ test/index.js
@@ -46,3 +46,41 @@ test('create g with a child', function(t) {
 
     t.end();
 });
+
+test('create circle with attributes', function(t) {
+    t.plan(2);
+
+    var testElement = crelns('http://www.w3.org/2000/svg', 'circle', {'class':'mycircle',cx:50,cy:50,r:25});
+
+    t.deepEqual(
+        testElement.className,
+        {animVal:'mycircle',baseVal:'mycircle'}
+    );
+
+    t.deepEqual(
+        testElement.cx.baseVal,
+        {value:50,valueInSpecifiedUnits:50}
+    );
+
+    t.end();
+});
+
+test('modify existing circle attributes', function(t) {
+    t.plan(2);
+
+    var testElement = crelns('http://www.w3.org/2000/svg', 'circle', {'class':'mycircle',cx:50,cy:50,r:25});
+
+    t.deepEqual(
+        testElement.r.baseVal,
+        {value:25,valueInSpecifiedUnits:25}
+    );
+
+    testElement = crelns('http://www.w3.org/2000/svg', testElement, {r:45});
+
+    t.deepEqual(
+        testElement.r.baseVal,
+        {value:45,valueInSpecifiedUnits:45}
+    );
+
+    t.end();
+});
